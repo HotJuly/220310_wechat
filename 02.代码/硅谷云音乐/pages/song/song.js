4c39ea52328bb2632e59da034d911aea6a32c48a
HotJuly <949588380@qq.com>
2022-09-26 15:58:51 +0800

    55.song页面播放歌曲功能实现

--- 02.代码/硅谷云音乐/pages/song/song.js
+++ 02.代码/硅谷云音乐/pages/song/song.js
@@ -7,7 +7,38 @@ Page({
   data: {
 
     // 用于存储当前歌曲页面的歌曲详情
-    songObj:{}
+    songObj:{},
+
+    // 用于存储当前歌曲的音频链接
+    musicUrl:"",
+
+    // 用于控制当前页面C3效果的状态
+    // 注意:当前项目进入song页面的时候,不会自动播放歌曲
+    isPlay:false
+  },
+
+  // 用于监视用户点击播放按钮操作
+  handlePlay(){
+    // console.log('handlePlay')
+
+    // 1.获取背景音频管理器对象
+    const backgroundAudioManager = wx.getBackgroundAudioManager();
+
+    if(this.data.isPlay){
+      backgroundAudioManager.pause();
+      
+    }else{
+  
+      // 2.给背景音频管理器对象添加src和title属性
+      backgroundAudioManager.src=this.data.musicUrl;
+  
+      backgroundAudioManager.title=this.data.songObj.name;
+  
+    }
+
+    this.setData({
+      isPlay:!this.data.isPlay
+    })
   },
 
   /**
@@ -29,6 +60,16 @@ Page({
     // 通过API的形式动态注入控制当前导航栏的展示标题
     wx.setNavigationBarTitle({
       title:this.data.songObj.name
+    })
+
+    const result1 = await this.$myAxios('/song/url',{
+      id:songId
+    })
+
+    const musicUrl = result1.data[0].url;
+
+    this.setData({
+      musicUrl
     })
   },
 
